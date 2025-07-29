bcc4ffb820a1ca3db15cd2f555cd1b998375435d
A. Liza Louni <a.lizz.louni@gmail.com>
2025-07-29 00:12:58 +0000

    use this lace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import { FeaturedProducts } from '@/components/home/featured-products';
 import { Testimonials } from '@/components/home/testimonials';
 import { InstagramFeed } from '@/components/home/instagram-feed';
 import { PromoBanner } from '@/components/home/promo-banner';
-import { DrippyChocolateBorder } from '@/components/ui/drippy-chocolate-border';
+import { LaceBorder } from '@/components/ui/lace-border';
 import { StyleQuiz } from '@/components/home/style-quiz';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
-      <DrippyChocolateBorder />
+      <LaceBorder />
       <FeaturedProducts />
       <StyleQuiz />
       <Testimonials />
