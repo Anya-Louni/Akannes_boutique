56b82187760fa579e0c77aa847fdf4a0254a5f1a
Anya Louni <a.lizz.louni@gmail.com>
2025-08-03 22:25:57 +0100

    minor changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,19 +4,21 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "akkanes-magical-boutique",
-  "appId": "1:510576374727:web:c492fbe7c16fa8fef69fe2",
-  "storageBucket": "akkanes-magical-boutique.firebasestorage.app",
-  "apiKey": "AIzaSyAd2O7hI9IyaE96e1Q_G0WUW1q38svA9Tg",
-  "authDomain": "akkanes-magical-boutique.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "510576374727"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAd2O7hI9IyaE96e1Q_G0WUW1q38svA9Tg",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "akkanes-magical-boutique.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "akkanes-magical-boutique",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "akkanes-magical-boutique.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "510576374727",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:510576374727:web:c492fbe7c16fa8fef69fe2",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
 };
 
 // Initialize Firebase
 let app;
 if (!getApps().length) {
   app = initializeApp(firebaseConfig);
+} else {
+  app = getApps()[0];
 }
 
 const db = getFirestore(app);
